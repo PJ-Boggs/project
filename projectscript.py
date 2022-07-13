31360458ac6de917e39ae33b9bb38b0a83f74e05
PJ-Boggs <101399846+PJ-Boggs@users.noreply.github.com>
2022-07-13 17:11:38 +0100

    Update projectscript.py

--- projectscript.py
+++ projectscript.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import arcpy
-from arcpy import env
-arcpy.env.overwriteOutput = False
 
-# Set path and gdb name as appropriate
-arcpy.env.workspace = "C:/Users/pjbog/project/DATA/project.gdb"
-premises =
-
-# 
+arcpy.CreateFileGDB_management("C:/Users/pjbog/project/DATA", "myfgdb.gdb")
