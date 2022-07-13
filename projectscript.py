e5fec5d057f4cae3e5c9aba09def67129e0ddf08
PJ-Boggs <101399846+PJ-Boggs@users.noreply.github.com>
2022-07-13 15:42:58 +0100

    Geodatabase Created

---
+++ projectscript.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import arcpy
+from arcpy import env
+arcpy.env.overwriteOutput = False
+
+# Set path and gdb name as appropriate
+arcpy.env.workspace = "C:/Users/pjbog/project/DATA/project.gdb"
+premises =
+
+# 
