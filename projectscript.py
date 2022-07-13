f74c525e6e396d3fde5c1a2d9c258187a2ab7316
PJ-Boggs <101399846+PJ-Boggs@users.noreply.github.com>
2022-07-13 17:48:37 +0100

    Update projectscript.py

--- projectscript.py
+++ projectscript.py
@@ -1,3 +1,9 @@
 import arcpy
 
-arcpy.CreateFileGDB_management("C:/Users/pjbog/project/DATA", "myfgdb.gdb")
+# Create a new File Geo-database (fgdb) to both work from and store data
+arcpy.CreateFileGDB_management("C:/Users/pjbog/project/DATA", "ReleaseSheet.gdb")
+
+# Set new fgdb as workspace
+arcpy.env.workspace = "C:/Users/pjbog/project/DATA/ReleaseSheet.gdb"
+# Export Demand_Points file as a copy to work from
+arcpy.FeatureClassToFeatureClass_conversion("Demand_Points", "C:/Users/pjbog/project/DATA/project.gdb", "ReleaseSheetPrems")
